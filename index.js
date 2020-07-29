0ed1c76a865916daad3da6333d06f79edfa0a41d
ruben-ruiz <r.ruiz24@outlook.com>
2020-07-29 12:08:43 -0400

    uploading current state of the project, need to still add realtime database

--- index.js
+++ index.js
@@ -34,25 +34,27 @@ function render(newBook) {
     let bookIndex = myLibrary.indexOf(newBook);
     let div = document.createElement('div');
     div.innerHTML = `<div class="library-books book-index${bookIndex}">
-                    <div class="library-books__prop book-title">${newBook.title}</div>
-                    <div class="library-books__prop book-author">${newBook.author}</div>
-                    <div class="library-books__prop book-pages">${newBook.pages}</div>
-                    <div class="library-books__prop book-status">
-                        <input type="checkbox" class="book-status__input">
-                            <span class="book-status__input-span">Yes</span>
-                            <span class="book-status__input-span">No</span>
-                    </div>
-                    <div class="library-books__prop">
-                        <button class="book-delete">
-                            <svg id="deleteBook-svg" version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                                <title>cancel-circle</title>
-                                <path d="M16 0c-8.837 0-16 7.163-16 16s7.163 16 16 16 16-7.163 16-16-7.163-16-16-16zM16 29c-7.18 0-13-5.82-13-13s5.82-13 13-13 13 5.82 13 13-5.82 13-13 13z"></path>
-                                <path d="M21 8l-5 5-5-5-3 3 5 5-5 5 3 3 5-5 5 5 3-3-5-5 5-5z"></path>
-                            </svg>
-                        </button>
-                    </div>
-                </div>`;
+                        <div class="library-books__prop book-title">${newBook.title}</div>
+                        <div class="library-books__prop book-author">${newBook.author}</div>
+                        <div class="library-books__prop book-pages">${newBook.pages}</div>
+                        <div class="library-books__prop book-status">
+                            <input type="checkbox" class="book-status__input">
+                                <span class="book-status__input-span">Yes</span>
+                                <span class="book-status__input-span">No</span>
+                        </div>
+                        <div class="library-books__prop book-remove">
+                            <button class="book-delete">
+                                <svg id="deleteBook-svg" version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                                    <title>cancel-circle</title>
+                                    <path d="M16 0c-8.837 0-16 7.163-16 16s7.163 16 16 16 16-7.163 16-16-7.163-16-16-16zM16 29c-7.18 0-13-5.82-13-13s5.82-13 13-13 13 5.82 13 13-5.82 13-13 13z"></path>
+                                    <path d="M21 8l-5 5-5-5-3 3 5 5-5 5 3 3 5-5 5 5 3-3-5-5 5-5z"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>`;
     document.querySelector('.library').append(div);
+
+    firebase.database().ref("book").set(document.getElementsByClassName("library-books").value);
 }
 
 
