6607b2a5ba07e2ac9593fb60e77036429ad5814c
ruben-ruiz <r.ruiz24@outlook.com>
2020-07-30 14:33:42 -0400

    finalized adding books to library on page load as well as adding additional books using add book button

--- index.js
+++ index.js
@@ -44,11 +44,20 @@
 
     updates["/newBook/" + key] = book;
     firebase.database().ref().update(updates);
-    render();
+
+    let bookArray = [];
+    firebase.database().ref('newBook').limitToLast(1).once('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            let childKey = childSnapshot.key;
+            let childData = childSnapshot.val();
+            bookArray.push(Object.values(childData));
+        })
+        
+        addToLibrary(bookArray);
+    });
  }
 
  function render() {
-    let libraryContainer = document.getElementById("library");
     let bookArray = [];
     firebase.database().ref('newBook').once('value', function(snapshot) {
         snapshot.forEach(function(childSnapshot) {
@@ -56,55 +65,57 @@
             let childData = childSnapshot.val();
             bookArray.push(Object.values(childData));
         })
-
-        for(let i = 0; i < bookArray.length; i++) {
-            let author = bookArray[i][0];
-            let keyData = bookArray[i][1];
-            let pages = bookArray[i][2];
-            let title = bookArray[i][3];
-
-            let libraryDiv = document.createElement('div');
-            libraryDiv.setAttribute("class", "library-books");
-            libraryDiv.setAttribute("data-key", keyData);
-
-            let titleDiv = document.createElement('div');
-            titleDiv.setAttribute("class", "library-books__prop book-title");
-            titleDiv.innerHTML = title;
-
-            let authorDiv = document.createElement('div');
-            authorDiv.setAttribute("class", "library-books__prop book-author");
-            authorDiv.innerHTML = author;
-
-            let pagesDiv = document.createElement('div');
-            pagesDiv.setAttribute("class", "library-books__prop book-pages");
-            pagesDiv.innerHTML = pages;
-
-            let statusDiv = document.createElement('div');
-            statusDiv.setAttribute("class", "library-books__prop book-status");
-            statusDiv.innerHTML =  `<input type="checkbox" class="book-status__input">
-                                    <span class="book-status__input-span">Yes</span>
-                                    <span class="book-status__input-span">No</span>`;
-
-            let removeDiv = document.createElement('div');
-            removeDiv.setAttribute("class", "library-books__prop book-remove");
-            removeDiv.innerHTML = `<button class="book-delete">
-                                        <svg id="deleteBook-svg" version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                                            <title>cancel-circle</title>
-                                            <path d="M16 0c-8.837 0-16 7.163-16 16s7.163 16 16 16 16-7.163 16-16-7.163-16-16-16zM16 29c-7.18 0-13-5.82-13-13s5.82-13 13-13 13 5.82 13 13-5.82 13-13 13z"></path>
-                                            <path d="M21 8l-5 5-5-5-3 3 5 5-5 5 3 3 5-5 5 5 3-3-5-5 5-5z"></path>
-                                        </svg>
-                                    </button>`;
-
-
-
-            libraryContainer.append(libraryDiv);
-            libraryDiv.append(titleDiv);
-            libraryDiv.append(authorDiv);
-            libraryDiv.append(pagesDiv);
-            libraryDiv.append(statusDiv);
-            libraryDiv.append(removeDiv);
-        }
+        
+        addToLibrary(bookArray);
     });
+ }
+
+ function addToLibrary(bookArr) {
+    for(let i = 0; i < bookArr.length; i++) {
+        let author = bookArr[i][0];
+        let keyData = bookArr[i][1];
+        let pages = bookArr[i][2];
+        let title = bookArr[i][3];
+
+        let libraryDiv = document.createElement('div');
+        libraryDiv.setAttribute("class", "library-books");
+        libraryDiv.setAttribute("data-key", keyData);
+
+        let titleDiv = document.createElement('div');
+        titleDiv.setAttribute("class", "library-books__prop book-title");
+        titleDiv.innerHTML = title;
+
+        let authorDiv = document.createElement('div');
+        authorDiv.setAttribute("class", "library-books__prop book-author");
+        authorDiv.innerHTML = author;
+
+        let pagesDiv = document.createElement('div');
+        pagesDiv.setAttribute("class", "library-books__prop book-pages");
+        pagesDiv.innerHTML = pages;
+
+        let statusDiv = document.createElement('div');
+        statusDiv.setAttribute("class", "library-books__prop book-status");
+        statusDiv.innerHTML =  `<input type="checkbox" class="book-status__input">
+                                <span class="book-status__input-span">Yes</span>
+                                <span class="book-status__input-span">No</span>`;
+
+        let removeDiv = document.createElement('div');
+        removeDiv.setAttribute("class", "library-books__prop book-remove");
+        removeDiv.innerHTML = `<button class="book-delete">
+                                    <svg id="deleteBook-svg" version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                                        <title>cancel-circle</title>
+                                        <path d="M16 0c-8.837 0-16 7.163-16 16s7.163 16 16 16 16-7.163 16-16-7.163-16-16-16zM16 29c-7.18 0-13-5.82-13-13s5.82-13 13-13 13 5.82 13 13-5.82 13-13 13z"></path>
+                                        <path d="M21 8l-5 5-5-5-3 3 5 5-5 5 3 3 5-5 5 5 3-3-5-5 5-5z"></path>
+                                    </svg>
+                                </button>`;
+
+        libraryContainer.append(libraryDiv);
+        libraryDiv.append(titleDiv);
+        libraryDiv.append(authorDiv);
+        libraryDiv.append(pagesDiv);
+        libraryDiv.append(statusDiv);
+        libraryDiv.append(removeDiv);
+    }
  }
 
  function removeBook(e) {
@@ -133,3 +144,4 @@
  let bookSection = document.querySelector('.library');
 
  let myLibrary = [];
+ let libraryContainer = document.getElementById("library");
